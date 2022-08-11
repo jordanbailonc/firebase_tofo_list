80b319a328f292748fe3f017f464a921504a712b
Jordan Bailon Cercado <jordanfbc@hotmail.com>
2022-08-11 21:52:15 +0200

    Make the task editable & update them

--- Fartz Project/firebase.js
+++ Fartz Project/firebase.js
@@ -8,6 +8,8 @@ import {
   onSnapshot,
   deleteDoc,
   doc,
+  getDoc,
+  updateDoc
 } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -40,3 +42,7 @@ export const onGetTasks = (callback) =>
   onSnapshot(collection(db, "tasks"), callback);
 
 export const deleteTask = (id) => deleteDoc(doc(db, "tasks", id));
+
+export const getTask = id => getDoc(doc(db,"tasks", id));
+
+export const updateTask =(id, newFields) => updateDoc(doc(db,'tasks',id),newFields)
--- Fartz Project/index.js
+++ Fartz Project/index.js
@@ -1,8 +1,18 @@
 //This will manipulates the information added by user
-import { saveTask, getTasks, onGetTasks, deleteTask} from "./firebase.js";
+import {
+  saveTask,
+  getTasks,
+  onGetTasks,
+  deleteTask,
+  getTask,
+  updateTask,
+} from "./firebase.js";
 
 const taskForm = document.getElementById("task-form");
 const tasksContainer = document.getElementById("tasks-container");
+
+let editStatus = false;
+let id ='';
 
 /*Once the program is open load the db data from Firebase async
  */
@@ -17,36 +27,59 @@ window.addEventListener("DOMContentLoaded", async () => {
                 <h3>${task.title}</h3>
                 <p>${task.description}</p>
                 <button class='btn-delete' data-id="${doc.id}">Delete</button>
-            </div>
+                <button class='btn-edit' data-id="${doc.id}">Edit</button>
+                </div>
             `;
     });
 
     tasksContainer.innerHTML = html;
 
-    const btnsDelete = tasksContainer.querySelectorAll('.btn-delete');
+    const btnsDelete = tasksContainer.querySelectorAll(".btn-delete");
 
     btnsDelete.forEach((btn) => {
       btn.addEventListener("click", ({ target: { dataset } }) => {
-        deleteTask(dataset.id)
+        deleteTask(dataset.id);
       });
-    })
+    });
+
+    //list of edit buttons
+    const btnsEdit = tasksContainer.querySelectorAll(".btn-edit");
+    btnsEdit.forEach((btn) => {
+      btn.addEventListener("click", async (e) => {
+        const doc = await getTask(e.target.dataset.id);
+        const task = doc.data();
+
+        taskForm["task-title"].value = task.title;
+        taskForm["task-description"].value = task.description;
+
+        editStatus = true;
+        id = e.target.dataset.id;
+        taskForm['btn-task-save'].innerText= 'Update';
+      });
+    });
   });
 });
 
-
 taskForm.addEventListener("submit", (e) => {
   //When it's loaded all information stop reading
-e.preventDefault();
+  e.preventDefault();
 
   //this get the info input
-const title = taskForm["task-title"];
-const description = taskForm["task-description"];
+  const title = taskForm["task-title"];
+  const description = taskForm["task-description"];
 
   /*this print it in console
     console.log(title.value +' '+ description.value)*/
-if(title.value!=="" && description.value!==""){
-    saveTask(title.value, description.value)
-}
+  if (!editStatus) {
+    if (title.value !== "" || description.value !== "") {
+      saveTask(title.value, description.value);
+    }else{
+        alert('To add a new task is necessary some information in title and description')
+    }
+  } else {
+      updateTask(id, {title: title.value, description: description.value});    
+  }
+
   //makes the textbox empty again
-taskForm.reset();
+  taskForm.reset(id);
 });
