1510323097567c8ed7133a3066c4d00cc033dda2
Jordan Bailon Cercado <jordanfbc@hotmail.com>
2022-08-11 16:08:01 +0200

    Separation of code

---
+++ firebase.js
@@ -0,0 +1,19 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-app.js";
+import {  } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBqsIpLlQvEDOlfLmjjoY90nNc7Z4s8aeg",
+    authDomain: "to-do-list-34fab.firebaseapp.com",
+    projectId: "to-do-list-34fab",
+    storageBucket: "to-do-list-34fab.appspot.com",
+    messagingSenderId: "48625624949",
+    appId: "1:48625624949:web:8e0d32cd9b96219de0d53c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ index.js
@@ -0,0 +1,5 @@
+//This will manipulates the  info
+
+window.addEventListener('DOMContentLoaded', ()=>{
+    console.log('works')
+})
