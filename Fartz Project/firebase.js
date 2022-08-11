b773dacf6cec844f6f3fa094d465247687b13a3d
Jordan Bailon Cercado <jordanfbc@hotmail.com>
2022-08-11 19:33:10 +0200

    Implementation of the delete option and correction of addition of empty settings

--- Fartz Project/firebase.js
+++ Fartz Project/firebase.js
@@ -1,34 +1,42 @@
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-app.js";
-import { getFirestore, addDoc,collection, getDocs, onSnapshot} from "https://www.gstatic.com/firebasejs/9.9.2/firebase-firestore.js";
+import {
+  getFirestore,
+  addDoc,
+  collection,
+  getDocs,
+  onSnapshot,
+  deleteDoc,
+  doc,
+} from "https://www.gstatic.com/firebasejs/9.9.2/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBqsIpLlQvEDOlfLmjjoY90nNc7Z4s8aeg",
-    authDomain: "to-do-list-34fab.firebaseapp.com",
-    projectId: "to-do-list-34fab",
-    storageBucket: "to-do-list-34fab.appspot.com",
-    messagingSenderId: "48625624949",
-    appId: "1:48625624949:web:8e0d32cd9b96219de0d53c"
+  apiKey: "AIzaSyBqsIpLlQvEDOlfLmjjoY90nNc7Z4s8aeg",
+  authDomain: "to-do-list-34fab.firebaseapp.com",
+  projectId: "to-do-list-34fab",
+  storageBucket: "to-do-list-34fab.appspot.com",
+  messagingSenderId: "48625624949",
+  appId: "1:48625624949:web:8e0d32cd9b96219de0d53c",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const db = getFirestore()
-
+const db = getFirestore();
 
 //This function can be moved and use by other parts from code
 //by using export
-export const saveTask = (title, description)=>
-    addDoc(collection(db, "tasks"),{title,description})
+export const saveTask = (title, description) =>
+  addDoc(collection(db, "tasks"), { title, description });
 
 //using get docs it recives all the colection from the db
 //with name: 'tasks' from firebase
-export const getTasks = () => getDocs(collection(db,'tasks'))
+export const getTasks = () => getDocs(collection(db, "tasks"));
 
-export const onGetTasks =(callback)=> onSnapshot(collection(db,'tasks'),callback)
+export const onGetTasks = (callback) =>
+  onSnapshot(collection(db, "tasks"), callback);
 
+export const deleteTask = (id) => deleteDoc(doc(db, "tasks", id));
--- Fartz Project/index.js
+++ Fartz Project/index.js
@@ -1,49 +1,52 @@
 //This will manipulates the information added by user
-import { saveTask, getTasks, onGetTasks} from './firebase.js'
+import { saveTask, getTasks, onGetTasks, deleteTask} from "./firebase.js";
 
-const taskForm = document.getElementById('task-form')
-const tasksContainer = document.getElementById('tasks-container')
+const taskForm = document.getElementById("task-form");
+const tasksContainer = document.getElementById("tasks-container");
 
 /*Once the program is open load the db data from Firebase async
-*/
-window.addEventListener('DOMContentLoaded', async () => {
-    //const querySnapshot = await getTasks()
+ */
+window.addEventListener("DOMContentLoaded", async () => {
+  onGetTasks((querySnapshot) => {
+    let html = "";
 
-    onGetTasks((querySnapshot) => {
-        let html = '';
-        
-        querySnapshot.forEach((doc) => {
-            const task = doc.data()
-            html += `
+    querySnapshot.forEach((doc) => {
+      const task = doc.data();
+      html += `
             <div>
-            <h3>${task.title}</h3>
-            <p>${task.description}</p>
+                <h3>${task.title}</h3>
+                <p>${task.description}</p>
+                <button class='btn-delete' data-id="${doc.id}">Delete</button>
             </div>
             `;
-        });
+    });
 
-        tasksContainer.innerHTML = html;
+    tasksContainer.innerHTML = html;
+
+    const btnsDelete = tasksContainer.querySelectorAll('.btn-delete');
+
+    btnsDelete.forEach((btn) => {
+      btn.addEventListener("click", ({ target: { dataset } }) => {
+        deleteTask(dataset.id)
+      });
     })
+  });
 });
 
 
+taskForm.addEventListener("submit", (e) => {
+  //When it's loaded all information stop reading
+e.preventDefault();
 
+  //this get the info input
+const title = taskForm["task-title"];
+const description = taskForm["task-description"];
 
-
-
-taskForm.addEventListener('submit', (e) => {
-    //When it's loaded all information stop reading
-    e.preventDefault()
-
-    //this get the info input
-    const title = taskForm['task-title']
-    const description = taskForm['task-description']
-
-    /*this print it in console
+  /*this print it in console
     console.log(title.value +' '+ description.value)*/
-
+if(title.value!=="" && description.value!==""){
     saveTask(title.value, description.value)
-
-    //makes the textbox empty again
-    taskForm.reset()
-})
+}
+  //makes the textbox empty again
+taskForm.reset();
+});
